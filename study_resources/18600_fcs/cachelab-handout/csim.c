3641a95ac2ceca52eeee0ab636975a0a39094cec
kshitij.khode <kshitij.khode@gmail.com>
2017-11-06 14:46:30 -0500

    [ACADEMICS] Cachelab initial commit ...
    [SCRIPTS] Misc changes in setting scripts

---
+++ study_resources/18600_fcs/cachelab-handout/csim.c
@@ -0,0 +1,15 @@
+/*
+ * csim.c - A cache simulator that implements cache with LRU policy
+ *   for replacement.
+ *
+ * You must implement this file.
+ */
+#include <stdio.h>
+#include "cache.h"
+#include "csim.h"
+#include "trace-stream.h"
+
+sim_result_t runSimulator(cache_config_t* config) {
+  sim_result_t results;
+  return results;
+}
---
+++ study_resources/18600_fcs/cachelab-handout/msim.c
@@ -0,0 +1,15 @@
+/*
+ * msim.c - A cache simulator that implements LRU and MSI protocol for
+ *   cache coherency.
+ *
+ * You must implement this file.
+ */
+#include <stdio.h>
+#include "cache.h"
+#include "msim.h"
+#include "trace-stream.h"
+
+sim_results_t runSimulator(cache_config_t* config) {
+  sim_results_t results;
+  return results;
+}
